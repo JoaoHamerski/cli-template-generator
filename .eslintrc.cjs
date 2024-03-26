9f2e7cff4b33bd15737e603e8b3993b93930820d
João Antônio Hamerski Copetti <joaohamerski@hotmail.com>
2024-03-26 14:17:20 -0300

    chore: init

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-explicit-any': [0],
+    '@typescript-eslint/no-unused-vars': [1],
+  },
+};
