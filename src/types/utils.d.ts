2e7979018938eb28ca12843942ebe0e6bfab73d7
João Antônio Hamerski Copetti <joaohamerski@hotmail.com>
2024-03-28 15:40:22 -0300

    refactor: refact type declaration structure

---
+++ src/types/utils.d.ts
@@ -0,0 +1,7 @@
+import type { PromptBuilt, TemplatesFileConfig, Template } from './template';
+
+export type GetConfigFn = (filepath: string) => Promise<TemplatesFileConfig>;
+export type AskForTemplateFn = (templates: Template[]) => Promise<Template>;
+export type BuildTemplatePromptsFn = (template: Template) => PromptBuilt[];
+export type GetFilenameFn = (filepath: string) => string;
+export type WorkDirFn = (joinPath?: string) => string;
