21394f96fdd9fcf5b2a2f33dd60993b1e6e48cb4
João Antônio Hamerski Copetti <joaohamerski@hotmail.com>
2024-03-28 15:43:39 -0300

    feat: add rest params to workDir function

--- src/types/utils.d.ts
+++ src/types/utils.d.ts
@@ -4,4 +4,4 @@ export type GetConfigFn = (filepath: string) => Promise<TemplatesFileConfig>;
 export type AskForTemplateFn = (templates: Template[]) => Promise<Template>;
 export type BuildTemplatePromptsFn = (template: Template) => PromptBuilt[];
 export type GetFilenameFn = (filepath: string) => string;
-export type WorkDirFn = (joinPath?: string) => string;
+export type WorkDirFn = (...joinPath: string[]) => string;
--- src/utils.ts
+++ src/utils.ts
@@ -3,12 +3,12 @@ import { WorkDirFn } from './types';
 import { cwd } from 'node:process';
 import path from 'node:path';
 
-export const workDir: WorkDirFn = (joinPath) => {
-  if (!joinPath) {
+export const workDir: WorkDirFn = (...joinPaths) => {
+  if (!joinPaths.length) {
     return cwd();
   }
 
-  return path.join(cwd(), joinPath);
+  return path.join(cwd(), ...joinPaths);
 };
 
 export const getFilename = (filepath: string) => {
