74369a3c9b640bdc85243082d8ac82463cfac929
João Antônio Hamerski Copetti <joaohamerski@outlook.com>
2024-03-28 15:40:22 -0300

    refactor: refact type declaration structure

---
+++ src/types/template.d.ts
@@ -0,0 +1,28 @@
+export interface Prompt {
+  type: 'input' | 'autocomplete';
+  message?: string;
+  options?: string[];
+}
+
+export type Answer = Record<string, string>;
+
+export interface PromptBuilt extends Prompt {
+  name: string;
+}
+
+export interface Template {
+  dest: string;
+  name: string;
+  source: string;
+  prompts: Record<string, Prompt>;
+}
+
+export interface TemplatesFileConfig {
+  templates: Template[];
+}
+
+export type TemplateConfigFn = () => Promise<TemplateConfig>;
+export type TemplateConfig = {
+  filepath: string;
+  templates: Template[];
+};
