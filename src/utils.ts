41fc56ce09dea629dbc7b1a150c44d8b413d7b1f
João Antônio Hamerski Copetti <joaohamerski@hotmail.com>
2024-04-01 19:58:51 -0300

    feat: add getFilePath and move compile util methods

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,8 @@
-import { WorkDirFn } from './types';
+import type { WorkDirFn } from './types';
 
 import { cwd } from 'node:process';
 import path from 'node:path';
+import Handlebars from 'handlebars';
 
 export const workDir: WorkDirFn = (...joinPaths) => {
   if (!joinPaths.length) {
@@ -13,4 +14,12 @@ export const workDir: WorkDirFn = (...joinPaths) => {
 
 export const getFilename = (filepath: string) => {
   return filepath.split('/').pop() || null;
+};
+
+export const compile = (input: string, data: any) => {
+  return Handlebars.compile(input)(data);
+};
+
+export const getFilepathDir = (filepath: string) => {
+  return filepath!.substring(0, filepath!.lastIndexOf('/'));
 };
